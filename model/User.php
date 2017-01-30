796c1eb98dd4b39ded4f45b8eed24061c616b1fd
jmarsal <jmarsal@student.42.fr>
2017-01-31 00:10:41 +0100

    modification sur le criptage de la cle user envoyer dans le mail

--- model/User.php
+++ model/User.php
@@ -88,7 +88,7 @@ class User extends Model {
 	// Enregistre un nouvel user dans la bdd + send mail de confirmation
 	public function addUser($login, $email, $passwd) {
 		$con = new Model;
-		$cle = hash(sha256, microtime(TRUE)*100000);
+		$cle = hash('sha256', microtime(TRUE)*100000);
 
 		$st = $con->db->query("SELECT COUNT(*) FROM users 
 									WHERE login='".$this->login."'")->fetch();
